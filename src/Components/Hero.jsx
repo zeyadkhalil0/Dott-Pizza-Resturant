fb43ab0458698031138c7239bba84057bf1ca76a
ZeYad <zeyadkhalil8@gmail.com>
2025-10-30 21:32:04 +0300

    Feat

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,43 +14,48 @@ const Hero = () => {
   }, []);
 
 const settings = {
-    dots: true,
-    infinite: true,
-    speed: 600,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 3 },
+  dots: true,
+  infinite: true,
+  speed: 600,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
       },
-      {
-        breakpoint: 768,
-        settings: { slidesToShow: 2 },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
       },
-      {
-        breakpoint: 480,
-        settings: { slidesToShow: 1 },
-      },
-    ],
-  };
+    },
+  ],
+};
+
   return (
   
     <section className=" w-full overflow-x-hidden">
-      <main className="h-screen flex flex-col gap-6 py-20 bg-black/50">
+      <main className="h-full flex flex-col gap-6 py-20 bg-black/50">
         {/* Top */}
-        <div className="flex md:justify-between justify-around items-center md:px-12">
+        <div className="flex md:flex-row flex-col  items-center space-y-4 px-8 m-auto md:px-12">
           {/* image */}
-          <div className="Pizza md:w-64 md:h-64 w-26 h-26 drop-shadow-2xl">
+          <div className="Pizza md:w-64 md:h-64 w-36 h-36 drop-shadow-2xl">
             <img src={PizzaOven} alt="" className="rounded-full object-cover" />
           </div>
           {/* Text */}
-          <div className="md:w-2/3 w-1/2 space-y-4 text-white">
+          <div className="md:w-2/3 flex flex-col text-center items-center justify-between space-y-4 text-white">
             <h1 className="font-bold md:text-4xl text-md">Pizza Is Italiano</h1>
-            <p className="font-light text-sm w-4/5">
+            <p className="font-light  text-sm w-4/5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
               eaque laborum rerum earum tempora illo delectus quia, officiis non
               dolores, et culpa. Dicta nesciunt architecto sunt quos omnis velit
@@ -61,11 +66,11 @@ const settings = {
         </div>
 
         {/* Bottom Swiper */}
-        <div className="md:w-2/3 w-full py-4 m-auto space-x-20">
+        <div className="md:w-2/3 w-full py-4 m-auto">
                 <Slider {...settings}>
             {pizzas.map((pizza) => (
               <div className="Card-Container px-4">
-              <div className="flex flex-col items-center h-64 text-center justify-between  px-2 py-4 rounded-xl bg-black/30 backdrop-blur-2xl overflow-hidden">
+              <div className="flex flex-col  items-center h-64 text-center justify-between  px-2 py-4 rounded-xl bg-black/30 backdrop-blur-2xl overflow-hidden">
                 <img
                   src={`https://hero-data.vercel.app/${pizza.image}`}
                   alt={pizza.name}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(),tailwindcss()],server:{
+    host:true,
+    port:5173,
+    open:false,
+  },
 })
